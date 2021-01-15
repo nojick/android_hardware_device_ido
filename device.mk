b94667de1e7e265cda73ff0ad68a7ee15d798110
nojick <nojick@gmail.com>
2021-01-15 21:22:13 +0300

    msm8916: fix fingerprint
    when building the service, there was an error that the symbol was not declared, I partially redid the service, but the scanner was not configured. I decided to try prebuild from android 10 and it worked perfectly. So just delete it because it is no longer needed.

--- device.mk
+++ device.mk
@@ -69,7 +69,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/vendor/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.0-service.ido \
+    android.hardware.biometrics.fingerprint@2.1 \
     fingerprint.msm8916
 
 # FM
