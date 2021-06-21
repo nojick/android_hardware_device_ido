e92aa911f24ce1ae3a67dfe4f2a24b9d77a1717c
nojick <nojick@gmail.com>
2021-06-21 12:50:23 +0300

    ido: sensor header

--- sensors/Android.mk
+++ sensors/Android.mk
@@ -13,6 +13,9 @@ LOCAL_SRC_FILES := \
     multihal.cpp \
     SensorEventQueue.cpp \
 
+LOCAL_HEADER_LIBRARIES := \
+    libhardware_headers \
+
 LOCAL_SHARED_LIBRARIES := \
     libcutils \
     libdl \
